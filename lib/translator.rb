34a3bc3aa36a8488b5012eae5dd2a64608137507
Kevin Downs <kevin.downs04@gmail.com>
2019-10-28 20:30:32 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,9 +2,9 @@
 require "yaml"
 
 def load_library(file)
-  emoticons = YAML.load_file(file)
-  
-  pp emoticons
+  emoticons_library = YAML.load_file(file)
+  emoticon_hash = {}
+  pp emoticons_library
 end
 
 def get_japanese_emoticon
